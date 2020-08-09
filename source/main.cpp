7cce8cfc3fc0acc111d2047dcf86433a1390907a
Kozlov Danil <danilkozlov1003@gmail.com>
2020-08-09 17:15:52 +0700

    Add new case num. 4 'findInFile' in switch

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include "Resume.h"
+#include "findInFile.h"
 #include "readData.h"
 #include "writeInFile.h"
 
@@ -6,25 +7,31 @@ void setMenu() {
     std::cout << "1) Enter resume(s) in file\n";
     std::cout << "2) Check base-file\n";
     std::cout << "3) Count of created resume(s)\n";
+    std::cout << "4) Find person's resume in file\n";
 }
 
 int main() {
     int choiceOfMenu;
+Point:
     setMenu();
     std::cin >> choiceOfMenu;
     switch (choiceOfMenu) {
         case 1:
             startWriteData();
             std::string(100, '\n');
-            break;
+            goto Point;
         case 2:
             readFile();
             std::string(100, '\n');
-            break;
+            goto Point;
         case 3:
             currentCountOfResume();
             std::string(100, '\n');
-            break;
+            goto Point;
+        case 4:
+            findInFile();
+            std::string(100, '\n');
+            goto Point;
         default:
             break;
     }
